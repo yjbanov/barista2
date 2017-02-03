0a91ae0eea30009f3c93eb766958c8ca8db6f560
Yegor Jbanov <yjbanov@google.com>
2017-02-02 23:21:37 -0800

    apply DOM diffs

--- main.cpp
+++ main.cpp
@@ -7,24 +7,49 @@
 using namespace std;
 using namespace barista;
 
-class SampleApp : public StatelessWidget {
+class SampleAppState : public State {
  public:
-  SampleApp() : StatelessWidget() {}
-  shared_ptr<Node> Build() {
+  bool greet = true;
+  int rows = 5;
+
+  virtual shared_ptr<Node> Build() {
     auto container = El("div");
 
     auto message = El("div");
-    message->AddChild(Tx("Hello"));
+    if (greet) {
+      message->AddChild(Tx("Hello"));
+    } else {
+      message->AddChild(Tx("Ciao!!!"));
+    }
+
+    for (int i = 0; i < rows; i++) {
+      cout << "adding row " << i << endl;
+      auto row = El("div");
+      row->AddChild(Tx("row #" + to_string(i)));
+      message->AddChild(row);
+    }
 
     auto button = El("button");
-    button->AddEventListener("click", []() {
-      cout << "Clicked!" << endl;
+    button->AddEventListener("click", [&]() {
+      cout << "Clicked! " << greet << endl;
+      greet = !greet;
+      rows++;
+      ScheduleUpdate();
     });
     button->AddChild(Tx("Greeting"));
 
     container->AddChild(message);
     container->AddChild(button);
     return container;
+  }
+};
+
+class SampleApp : public StatefulWidget {
+ public:
+  SampleApp() : StatefulWidget() {}
+
+  virtual shared_ptr<State> CreateState() {
+    return make_shared<SampleAppState>();
   }
 };
 
