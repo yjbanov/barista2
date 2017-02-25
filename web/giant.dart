410e032bf195f7c86457998bd6555f1d734bd29f
Yegor Jbanov <yjbanov@google.com>
2017-02-24 16:32:40 -0800

    get ng2 bootstrap numbers

--- web/giant.dart
+++ web/giant.dart
@@ -1,6 +1,16 @@
+import 'dart:async';
+import 'dart:html' as html;
+import 'dart:js' as js;
+
 import 'package:angular2/platform/browser.dart';
 import 'giant_widgets.dart';
 
-main() {
-  bootstrap(GiantApp);
+main() async {
+  js.context.callMethod('inMain');
+  await new Future.delayed(new Duration(milliseconds: 50));
+
+  var bootStart = html.window.performance.now();
+  await bootstrap(GiantApp);
+  var bootEnd = html.window.performance.now();
+  print('>>> Bootstrap time: ${bootEnd - bootStart} ms');
 }
